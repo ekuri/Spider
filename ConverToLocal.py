b11cb157259016d5686cd87b44c7f76fb4d7ead3
ekuri <wuwy23@mail2.sysu.edu.cn>
2015-10-27 13:07:46 +0800

    add local converter

---
+++ ConverToLocal.py
@@ -0,0 +1,24 @@
+﻿import os
+import sys
+
+def main(argv):
+    if not (len(argv) == 2):
+        print 'Usage:  ' + argv[0] + '  [filename]'
+        return
+        
+    if not os.path.isfile(argv[1]):
+        print 'Not a file'
+        return
+        
+    print 'Converting to local...'
+    targetFileReader = open(argv[1], 'r')
+    fileLines = targetFileReader.readlines()
+    targetFileReader.close()
+
+    targetFileWriter = open('test.log', 'wb')
+    for line in fileLines:
+        print 'Writing: ' + line.strip()
+        targetFileWriter.write(line.strip() + '\n')
+
+if __name__ == '__main__':
+    main(sys.argv)
