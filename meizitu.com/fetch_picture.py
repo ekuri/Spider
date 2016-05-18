a2a43364ed287ec33322f234a0ab62ead08931ce
ekuri <ekuri2014@outlook.com>
2016-05-18 17:26:58 +0800

    update meizitu.com/fetch_picture.py, add wating check

--- meizitu.com/fetch_picture.py
+++ meizitu.com/fetch_picture.py
@@ -16,20 +16,25 @@ cursor = Database.cursor()
 cursor.execute('select id, url from pictures where status = "%s"' % 'new')
 row = cursor.fetchone()
 socket_timeout = 5
-while (row):
+while row:
     try:
+        update_cursor = Database.cursor()
+        update_cursor.execute('update pictures set status = "%s" where id = %d' % ('waiting', row[0]))
+        Database.commit()
         print row[1]
         picture_data = urllib2.urlopen(url = row[1], timeout = socket_timeout).read()
+
         sha1sum = hashlib.sha1(picture_data).hexdigest()
         filename = sha1sum + '.' + row[1].split('.')[-1]
         print filename
         picture_file = open(base_path + '/' + filename, 'wb')
         picture_file.write(picture_data)
-        update_cursor = Database.cursor()
+
         update_cursor.execute('update pictures set status = "%s", filename = "%s" where id = %d' % ('done', filename, row[0]))
         Database.commit()
         update_cursor.close()
 
+        cursor.execute('select id, url from pictures where status = "%s"' % 'new')
         row = cursor.fetchone()
     except KeyboardInterrupt:
         break
